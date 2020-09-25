de97ff912fb56697ab58a4a4adbb46d2474e8bba
Fedex832 <philippe.mariel832@gmail.com>
2020-09-25 16:31:00 +0200

    chore(exercice 1) initialize exercice 1

--- exercice-1/index.js
+++ exercice-1/index.js
@@ -1,1 +1,22 @@
 'use strict';
+
+/*
+ * Grid
+ * @param {number} xAxis
+ * @param {mumber} yAxis
+ */
+
+
+var Grid = function Grid(xAxis, yAxis) {
+  this.xAxis = xAxis;
+  this.yAxis = yAxis;
+}
+
+/*
+ * run
+ * @return this
+ */
+
+Grid.prototype.run = function() {
+  return this.Grid;
+}
