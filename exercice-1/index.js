8c4678b56272a4a6d5a62221ef7aa21fc6fe913a
Fedex832 <philippe.mariel832@gmail.com>
2020-09-25 16:40:30 +0200

    feat(grid) add method Grid

--- exercice-1/index.js
+++ exercice-1/index.js
@@ -6,7 +6,6 @@
  * @param {mumber} yAxis
  */
 
-
 var Grid = function Grid(xAxis, yAxis) {
   this.xAxis = xAxis;
   this.yAxis = yAxis;
@@ -18,5 +17,30 @@ var Grid = function Grid(xAxis, yAxis) {
  */
 
 Grid.prototype.run = function() {
-  return this.Grid;
+  this.matrix();
+  return this;
 }
+
+/*
+ * matrix
+ */
+
+ Grid.prototype.matrix = function() {
+    var matrix = document.createElement('table');
+
+    for(var i = 1; i <= this.xAxis; i++) {
+        var tr = document.createElement('tr');
+        for(var j = 1; j <= this.yAxis; j++) {
+            var td = document.createElement('td');
+            td.style.border = 'solid';
+            td.style.width = '20px';
+            td.style.height = '20px';
+            tr.appendChild(td);
+        }
+        matrix.appendChild(tr);
+    }
+    document.body.appendChild(matrix);
+}
+
+var grid1 = new Grid(5, 5);
+grid1.run();
