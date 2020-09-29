fa3f548fee8608f48c1437de850c23a6bf5a85e6
Fedex832 <philippe.mariel832@gmail.com>
2020-09-29 16:25:07 +0200

    feat(bar) add methode bar

--- exercice-2/index.js
+++ exercice-2/index.js
@@ -1,1 +1,29 @@
 'use strict';
+
+var DrawBar = function DrawBar(sum, nbr) {
+  this.sum = sum;
+  this.nbr = nbr;
+}
+
+DrawBar.prototype.run = function() {
+  this.bar();
+}
+
+DrawBar.prototype.bar = function() {
+  var barFull = document.createElement('div');
+  var barProgress = document.createElement('div');
+  var nbr = this.nbr;
+  var sum = this.sum;
+
+  barFull.style.width = '100%';
+  barFull.style.height = '30px';
+  barFull.style.border = 'solid';
+  barProgress.style.width = (nbr / sum) * 100 + '%'
+  barProgress.style.height = '100%'
+  barProgress.style.backgroundColor = 'purple';
+  barFull.appendChild(barProgress);
+  document.body.appendChild(barFull);
+}
+
+var bar1 = new DrawBar(100, 25);
+bar1.run();
