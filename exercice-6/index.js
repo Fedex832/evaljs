b6c44a785fe16f667928edebf8b85341f2415777
Fedex832 <philippe.mariel832@gmail.com>
2020-10-10 17:26:09 +0200

    feat(form) add methode form

--- exercice-6/index.js
+++ exercice-6/index.js
@@ -5,8 +5,21 @@ var MyNumberCheck = function MyNumberCheck() {
 }
 
 MyNumberCheck.prototype.run = function() {
-
+  this.form();
 }
 
-var number1 = new.MyNumberCheck();
+MyNumberCheck.prototype.form = function() {
+  var form = document.createElement('form');
+  var number = document.createElement('input');
+  number.setAttribute('type', 'text');
+  number.setAttribute('placeholder', 'Numero');
+  form.appendChild(number);
+  var submit = document.createElement('input');
+  submit.value = 'Confirmer';
+  submit.setAttribute('type', 'button');
+  form.appendChild(submit);
+  document.body.appendChild(form);
+}
+
+var number1 = new MyNumberCheck();
 number1.run();
