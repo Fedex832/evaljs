4f3364a7d3c907526c72b224287661cb2160f92b
Fedex832 <philippe.mariel832@gmail.com>
2020-10-10 17:42:04 +0200

    feat(check) add methode check

--- exercice-6/index.js
+++ exercice-6/index.js
@@ -6,6 +6,7 @@ var MyNumberCheck = function MyNumberCheck() {
 
 MyNumberCheck.prototype.run = function() {
   this.form();
+  this.check();
 }
 
 MyNumberCheck.prototype.form = function() {
@@ -19,6 +20,24 @@ MyNumberCheck.prototype.form = function() {
   submit.setAttribute('type', 'button');
   form.appendChild(submit);
   document.body.appendChild(form);
+}
+
+MyNumberCheck.prototype.check = function() {
+  var button = document.querySelector(('[type="button"]'));
+  var p = document.createElement('p');
+  button.addEventListener('click', event => {
+    p.innerHTML = '';
+    var numberCorrect = RegExp(/(06|07|01)\d{8}/);
+    var numberValue = document.querySelector('[placeholder="Numero"]').value;
+    if(numberCorrect.test(numberValue)) {
+      p.textContent = 'Numero valide';
+      p.style.color = 'green';
+    }else {
+      p.textContent = 'Numero invalide';
+      p.style.color = 'red';
+    }
+    document.body.appendChild(p);
+  })
 }
 
 var number1 = new MyNumberCheck();
