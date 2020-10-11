bfb7b2b8b8ed78f3d7e0d35eef2719e40bc86b2a
Fedex832 <philippe.mariel832@gmail.com>
2020-10-11 15:43:40 +0200

    feat(form) add methode form

--- exercice-7/index.js
+++ exercice-7/index.js
@@ -7,7 +7,24 @@ var MyAverage = function MyAverage(markArray) {
 }
 
 MyAverage.prototype.run = function() {
+  this.form();
+}
 
+MyAverage.prototype.form = function() {
+  var form = document.createElement('form');
+  var mark = document.createElement('input');
+  mark.setAttribute('type', 'text');
+  mark.setAttribute('placeholder', 'Note');
+  form.appendChild(mark);
+  var submit = document.createElement('input');
+  submit.value = 'Ajouter';
+  submit.setAttribute('type', 'button');
+  form.appendChild(submit);
+  var cal = document.createElement('input');
+  cal.value = 'Confirmer';
+  cal.setAttribute('type', 'button');
+  form.appendChild(cal);
+  document.body.appendChild(form);
 }
 
 var average1 = new MyAverage(markArray);
