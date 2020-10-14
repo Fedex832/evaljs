0ac0b5d521b6157e9bbb7e491ffb720d7c1c0475
Fedex832 <philippe.mariel832@gmail.com>
2020-10-14 20:05:58 +0200

    feat(select) add methode select and update methode grid

--- exercice-8/index.js
+++ exercice-8/index.js
@@ -13,11 +13,14 @@ var board = [[0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0],
             [0, 0, 0, 0, 0, 5, 5, 5, 5, 5, 0, 0],
             [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]];
 
+var colorShot = '';
+
 var PanPanPlouf = function PanPanPlouf() {
 }
 
 PanPanPlouf.prototype.run = function() {
   this.grid();
+  this.select();
 }
 
 PanPanPlouf.prototype.grid = function() {
@@ -29,11 +32,39 @@ PanPanPlouf.prototype.grid = function() {
     for (var j = 0; j < board[i].length; j++) {
       var td = document.createElement('td');
       td.style.border = 'solid';
+      if(board[i][j] == 1) {
+        td.classList.add('ship1')
+      }else if(board[i][j] == 2) {
+        td.classList.add('ship2')
+      }else if(board[i][j] == 3) {
+        td.classList.add('ship3')
+      }else if(board[i][j] == 4) {
+        td.classList.add('ship4')
+      }else if(board[i][j] == 4) {
+        td.classList.add('ship4')
+      }else if(board[i][j] == 5) {
+        td.classList.add('ship5')
+      }else {
+        td.classList.add('sea')
+      }
+
       tr.appendChild(td);
     }
     grid.appendChild(tr);
   }
   document.body.appendChild(grid);
+}
+
+PanPanPlouf.prototype.select = function() {
+  var td = document.querySelectorAll('td');
+  td.forEach(function(item){
+    item.addEventListener('click', event => {
+      event.target.classList.contains('sea') ? colorShot = 'blue' : colorShot = 'red';
+      event.target.style.backgroundColor = colorShot;
+      event.target.classList.add('shot');
+    })
+  });
+
 }
 
 var boat1 = new PanPanPlouf();
